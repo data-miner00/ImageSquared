72c959e4b274569a7eb3b254786d3c8f57167229
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-13 16:33:40 +0800

    feat: Use IoC container

--- ImageSquared/App.xaml.cs
+++ ImageSquared/App.xaml.cs
@@ -1,14 +1,45 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Autofac;
 
 namespace ImageSquared
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public sealed partial class App : Application, IDisposable
     {
+        private readonly IContainer container = ContainerConfig.Configure();
+
+        private bool isDisposed;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = this.container.Resolve<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            this.Dispose();
+
+            base.OnExit(e);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (this.isDisposed)
+            {
+                return;
+            }
+
+            this.container.Dispose();
+            this.isDisposed = true;
+        }
     }
 
 }
---
+++ ImageSquared/ContainerConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace ImageSquared
+{
+    internal static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainWindow>().SingleInstance();
+
+            return builder.Build();
+        }
+    }
+}
