d1a8c14702b5eab284ba98e0291b2f1123faa966
data-miner00 <data-miner00@users.noreply.github.com>
2024-11-10 08:46:43 +0800

    fix: Further simplify UI

--- ImageSquared/View/MainWindow.xaml.cs
+++ ImageSquared/View/MainWindow.xaml.cs
@@ -137,6 +137,8 @@ public partial class MainWindow : Window
         this.transformedBitmapImage = extendedImage;
 
         this.TransformedImage.Source = extendedImage;
+
+        this.btnSave_Click(sender, e);
     }
 
     private void SaveSelectedFile(string filePath)
