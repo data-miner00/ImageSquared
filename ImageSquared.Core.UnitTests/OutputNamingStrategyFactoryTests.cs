5b449ed5929142fdf418af75d36b2e2e1c566ab6
data-miner00 <data-miner00@users.noreply.github.com>
2024-11-14 07:44:17 +0800

    test: Add factory tests

---
+++ ImageSquared.Core.UnitTests/OutputNamingStrategyFactoryTests.cs
@@ -0,0 +1,34 @@
+﻿namespace ImageSquared.Core.UnitTests;
+
+using ImageSquared.Core.Models;
+using System;
+using Xunit;
+
+public sealed class OutputNamingStrategyFactoryTests
+{
+    [Fact]
+    public void Constructor_InvalidImageFormat_Throws()
+    {
+        Assert.Throws<ArgumentException>(() => new OutputNamingStrategyFactory(ImageFormat.None));
+    }
+
+    [Theory]
+    [InlineData(NamingStrategy.Guid, typeof(GuidNamingStrategy))]
+    [InlineData(NamingStrategy.Timestamp, typeof(TimestampNamingStrategy))]
+    public void Create_ValidNamingStrategy_InstanceCreatedAccordingly(NamingStrategy namingStrategy, Type namingStrategyImplType)
+    {
+        var factory = new OutputNamingStrategyFactory(ImageFormat.Jpg);
+
+        var namingStrategyImpl = factory.Create(namingStrategy);
+
+        Assert.IsType(namingStrategyImplType, namingStrategyImpl);
+    }
+
+    [Fact]
+    public void Create_InvalidNamingStrategy_Throws()
+    {
+        var factory = new OutputNamingStrategyFactory(ImageFormat.Png);
+
+        Assert.Throws<NotSupportedException>(() => factory.Create(default));
+    }
+}
